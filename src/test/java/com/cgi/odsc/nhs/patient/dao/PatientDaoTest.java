75c11e76c040038c43a04c2d1f41adea378b86c8
shaheenr <rabia.shaheen@cgi.com>
2017-06-20 23:48:36 +0100

    Updated PatientDao Testcases

--- src/test/java/com/cgi/odsc/nhs/patient/dao/PatientDaoTest.java
+++ src/test/java/com/cgi/odsc/nhs/patient/dao/PatientDaoTest.java
@@ -2,6 +2,8 @@ package com.cgi.odsc.nhs.patient.dao;
 
 import com.cgi.odsc.nhs.patient.domain.Patient;
 import com.cgi.odsc.nhs.patient.dao.IPatientDao;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,13 +11,20 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate4.HibernateTemplate;
-
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import static org.mockito.Mockito.verify;
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 /**
@@ -25,35 +34,49 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class PatientDaoTest {
 
-    @Autowired
-    HibernateTemplate hibernateTemplate;
+    @Mock
+    public HibernateTemplate hibernateTemplate;
+    @Mock
+    public SessionFactory sessionFactory;
+    @Mock
+    public DataSource dataSource;
 
     @Mock
-    IPatientDao patientDao;
-
+    public HibernateTransactionManager hibTransMan;
 
     @Before
     public void setUp(){
-        //patientDao= new PatientDao();
-        hibernateTemplate = new HibernateTemplate();
+
     }
+    @Mock
+    IPatientDao dao;
 
     @Test
     public void savePatient(){
+
         Patient patient = new Patient(1,"Alpha");
-        when(patientDao.saveOrUpdatePatient(patient)).thenReturn(patient);
-        patient = patientDao.saveOrUpdatePatient(patient);
-        assertEquals(new Long(1),new Long(patient.getId()));
+
+        doNothing().when(hibernateTemplate).saveOrUpdate(patient);
+        hibernateTemplate.saveOrUpdate(patient);
+
+        when(hibernateTemplate.get(Patient.class,patient.getId())).thenReturn(patient);
+        Patient actual = hibernateTemplate.get(Patient.class,patient.getId());
+
+        verify(hibernateTemplate).saveOrUpdate(patient);
+        assertSame(patient, actual);
     }
 
     @Test
     public void deletePatient(){
         Patient patient = new Patient(1,"Alpha");
-        //patient =patientDao.deletePatient(patient);
-        //assertEquals(null,patient);
-        when(patientDao.deletePatient(patient)).thenReturn(null);
-        patient = patientDao.deletePatient(patient);
-        assertEquals(null,patient);
+
+        doNothing().when(hibernateTemplate).delete(patient);
+        hibernateTemplate.delete(patient);
+
+        when(hibernateTemplate.get(Patient.class,patient.getId())).thenReturn(null);
+        Patient actual = hibernateTemplate.get(Patient.class,patient.getId());
+
+        assertSame(null, actual);
 
     }
     List<Patient> patients;
@@ -63,14 +86,14 @@ public class PatientDaoTest {
     public void listAllPatientsTest(){
         patients = new ArrayList<Patient>();
 
-        patients = patientDao.listAllPatients();
+        patients = hibernateTemplate.loadAll(Patient.class);
         assertEquals(0,patients.size());
         patients.add(Patient.builder().id(1).name("alpha").build());
         patients.add(Patient.builder().id(2).name("beta").build());
         patients.add(Patient.builder().id(3).name("theta").build());
 
-        Mockito.when(patientDao.listAllPatients()).thenReturn(patients);
-        patients = patientDao.listAllPatients();
+        Mockito.when(hibernateTemplate.loadAll(Patient.class)).thenReturn(patients);
+        patients = hibernateTemplate.loadAll(Patient.class);
         assertNotNull(patients);
         assertEquals(3,patients.size());
     }
