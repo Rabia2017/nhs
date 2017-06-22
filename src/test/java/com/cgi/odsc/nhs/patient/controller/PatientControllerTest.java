f47b18781d0171522044075f658ec14a11743918
shaheenr <rabia.shaheen@cgi.com>
2017-06-22 10:55:45 +0100

    getPatientsTest case ADDED

--- src/test/java/com/cgi/odsc/nhs/patient/controller/PatientControllerTest.java
+++ src/test/java/com/cgi/odsc/nhs/patient/controller/PatientControllerTest.java
@@ -1,26 +1,83 @@
 package com.cgi.odsc.nhs.patient.controller;
 
+import com.cgi.odsc.nhs.config.AppConfig;
+import com.cgi.odsc.nhs.config.DBConfig;
+import com.cgi.odsc.nhs.exception.NHSException;
+import com.cgi.odsc.nhs.patient.dao.IPatientDao;
+import com.cgi.odsc.nhs.patient.dao.PatientDao;
+import com.cgi.odsc.nhs.patient.domain.Patient;
+import com.cgi.odsc.nhs.patient.service.IPatientService;
 import com.cgi.odsc.nhs.patient.service.PatientService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.View;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 /**
  * Created by rabia on 18/06/17.
 */
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {AppConfig.class,DBConfig.class})
+@WebAppConfiguration
+@TestExecutionListeners(listeners = {DependencyInjectionTestExecutionListener.class})
 public class PatientControllerTest {
     @Mock
-    PatientService patientService;
+    IPatientService mockPatientService;
+
+    @Mock
+    IPatientDao patientDao;
+
+    @InjectMocks
+    @Resource
+    PatientController patientController;
+
+
+    MockMvc mockMvc;
+
+    @Mock
+    View mockView;
+
 
     @Before
-    public void SetUp(){}
+    public void setUp() throws NHSException{
+        patientDao = new PatientDao();
+        mockPatientService =new PatientService(patientDao);
+
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(patientController)
+                .setSingleView(mockView)
+                .build();
+    }
 
     @Test
-    public void getPatientsTest(){
+    public void getPatientsTest() throws Exception{
+        List<Patient> expectedPatients  =   asList(new Patient(1,"abc"));
+        when(mockPatientService.getPatients()).thenReturn(expectedPatients);
 
+        mockMvc.perform(get("/patients"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("patients",expectedPatients))
+                .andExpect(view().name("patients"));
     }
 
 }
